9227fe7270e4b2f33b8730b5bcb5f2476897ea88
EduardoMontanha <edumontanha14@gmail.com>
2016-06-22 13:16:25 -0300

    Adding "Sobre" page

--- app/app.js
+++ app/app.js
@@ -8,6 +8,11 @@ app.config(function($routeProvider, $locationProvider){
             templateUrl: 'app/views/home.html',
             controller: 'HomeCtrl'
         })
+        //Sobre
+        .when('/sobre', {
+            templateUrl: 'app/views/sobre.html',
+            controller: 'SobreCtrl'
+        })
         //Contact
         .when('/contato', {
             templateUrl: 'app/views/contato.html',
