c6cae4b65f1c436ad92c1b74d84936427cde02f1
EduardoMontanha <edumontanha14@gmail.com>
2017-10-02 15:04:54 -0300

    Change image source
    Change the image source when mouse is hover the link

---
+++ js/home.js
@@ -0,0 +1,51 @@
+let home = {
+    
+    imageHover: function () {
+        'use strict';
+        let $links = $("main a");
+        
+        if ($links.length) {
+            
+            $links.hover(function () {
+                
+                let $this = $(this),
+                    id = $this.closest("section").attr("id");
+                
+                $this.find("source[media*='768px']").attr("srcset", function () {
+                    return "img/home/" + id + "-icon.png";
+                });
+                
+            }, function () {
+                
+                let $this = $(this),
+                    id = $this.closest("section").attr("id");
+                
+                $this.find("source[media*='768px']").attr("srcset", function () {
+                    return "img/home/" + id + "-icon-grey.png";
+                });
+            });
+        }
+        
+        setTimeout(function () {
+            try {
+                home.imageHover();
+            } catch (e) {
+                console.err(e);
+            }
+        }, 200);
+    },
+    
+    init: function () {
+        'use strict';
+        
+        //Only runs for devices where width is equal or bigger than 768px
+        let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        
+        if (width >= 768) {
+            //Change image when mouse is hover the link
+            this.imageHover();
+        }
+    }
+};
+
+home.init();
