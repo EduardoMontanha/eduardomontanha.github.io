6af440d2f662a11b4d227c0c90721e202c0a39d3
EduardoMontanha <edumontanha14@gmail.com>
2017-10-09 13:26:57 -0300

    Preload Images
    Adding a function to preload the white images on tablet/desktop viewport.
    This will remove the flicker when the user hover the buttons for the first time.

--- js/home.js
+++ js/home.js
@@ -35,12 +35,29 @@ let home = {
         }, 200);
     },
     
+    preload: function () {
+        'use strict';
+        let images = new Array(),
+            imgSrc = [
+                "dev",
+                "ma",
+                "mu",
+            ];
+        
+        for (let i = 0; i < imgSrc.length; i++) {
+            images[i] = new Image();
+            images[i].src = "img/home/" + imgSrc[i] + "-icon.png";
+        }
+    },
+    
     init: function () {
         'use strict';
         //Only runs for devices where width is equal or bigger than 768px
         let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
         
         if (width >= 768) {
+            //Load images before the request function
+            this.preload();
             //Change image when mouse is hover the link
             this.imageHover();
         }
