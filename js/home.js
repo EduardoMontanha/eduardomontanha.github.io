f6942da92d2da2d44cb1e21f7d4bf9205200ffde
EduardoMontanha <edumontanha14@gmail.com>
2017-10-09 13:37:30 -0300

    Adding Comments
    Adding comments about methods functionality

--- js/home.js
+++ js/home.js
@@ -1,5 +1,8 @@
 let home = {
     
+    /**
+     * Change the image when user hovers one of the portfolio links.
+     */
     imageHover: function () {
         'use strict';
         let $links = $("main a");
@@ -35,6 +38,10 @@ let home = {
         }, 200);
     },
     
+    /**
+     * Load images before user interacts with the imageHover function.
+     * This will avoid the flicker the first time user hovers one of the links.
+     */
     preload: function () {
         'use strict';
         let images = new Array(),
@@ -56,9 +63,7 @@ let home = {
         let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
         
         if (width >= 768) {
-            //Load images before the request function
             this.preload();
-            //Change image when mouse is hover the link
             this.imageHover();
         }
     }
