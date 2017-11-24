bfce7b846ac5f786cf2f8f38054f0e3f987e38d9
EduardoMontanha <edumontanha14@gmail.com>
2017-11-24 19:58:41 -0200

    Adding smooth scrolling efect

--- js/index.js
+++ js/index.js
@@ -1,72 +1,33 @@
-let home = {
-    
-    /**
-     * Change the image when user hovers one of the portfolio links.
-     */
-    imageHover: function () {
+/* globals $, document, window */
+
+var behaviors = {
+    smoothScroll: function () {
         'use strict';
-        let $links = $("main a");
-        
-        if ($links.length) {
-            
-            let $this, id;
-            
-            $links.hover(function () {
+        $("a").on('click', function (event) {
+            //Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
                 
-                $this = $(this);
-                id = $this.closest("section").attr("id");
+                event.preventDefault();
                 
-                $this.find("source[media*='768px']").attr("srcset", function () {
-                    return "img/home/" + id + "-icon.png";
+                //Store hash
+                var hash = this.hash;
+                
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function () {
+                    //Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
                 });
-                
-            }, function () {
-                $this.find("source[media*='768px']").attr("srcset", function () {
-                    return "img/home/" + id + "-icon-grey.png";
-                });
-            });
-            
-            return;
-        }
-        
-        setTimeout(function () {
-            try {
-                home.imageHover();
-            } catch (e) {
-                console.err(e);
             }
-        }, 200);
-    },
-    
-    /**
-     * Load images before user interacts with the imageHover function.
-     * This will avoid the flicker the first time user hovers one of the links.
-     */
-    preload: function () {
-        'use strict';
-        let images = [],
-            imgSrc = [
-                "dev",
-                "ma",
-                "mu",
-            ];
-        
-        for (let i = 0; i < imgSrc.length; i++) {
-            images[i] = new Image();
-            images[i].src = "img/home/" + imgSrc[i] + "-icon.png";
-        }
+        });
     },
     
     init: function () {
         'use strict';
-        //Only runs for devices where width is equal or bigger than 768px
-        let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-        
-        if (width >= 768) {
-            this.preload();
-            this.imageHover();
-        }
+        this.smoothScroll();
     }
 };
 
-home.init();
+$(document).ready(function () {
+    behaviors.init();
+});
